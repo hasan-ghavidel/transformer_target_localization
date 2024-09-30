785d77bb4a1a9209cd13898fe5815cc376ed0eca
hasan-ghavidel <123900637+hasan-ghavidel@users.noreply.github.com>
2024-09-30 14:43:37 +0200

    Update config.py

--- code/config.py
+++ code/config.py
@@ -11,18 +11,18 @@ import sys
 
 # set path to project folder 
 # (to be adapted to your path inside Docker container where project folder is mounted)
-path_project = '/project/med/Hassan_Ghavidel/transformer_target_localization'
+path_project = '/project/med/Hassan_Ghavidel/transformer_target_localization_phantom'
 
 # path to data folder
 path_project_data = os.path.join(path_project, 'data')
 
-# path to model buiding code folder
+# path to model building code folder
 path_project_code = os.path.join(path_project, 'code')
 
 # path to results folder
 path_project_results = os.path.join(path_project, 'results')
 
-# add project auxiliary and models folder to Python path to be able to import self written modules from anywhere
+# add project auxiliary and models folder to Python path to be able to import self-written modules from anywhere
 sys.path.append(os.path.join(path_project_code, 'auxiliary'))
 sys.path.append(os.path.join(path_project_code, 'models'))
 # import utils
@@ -32,7 +32,7 @@ sys.path.append(os.path.join(path_project_code, 'models'))
 
 print('-------------------------------------------------')
 # GPU settings
-gpu_iden = 0  # None=CPU, 0,1,...=GPU (explictly set GPU ID)
+gpu_iden = 1  # None=CPU, 0,1,...=GPU (explicitly set GPU ID)
 if gpu_iden is not None:
     if torch.cuda.is_available():  
         print('GPUs are available!')
@@ -60,8 +60,9 @@ plot = True # whether to plot results when running scripts
 #%%
 # SET PARAMETERS FOR SCRIPTS
 
-# time at which run is started, used to save results
-start_time_string = time.strftime("%Y-%m-%d-%H:%M:%S")  
+#start_time_string = time.strftime("%Y-%m-%d-%H:%M:%S")
+custom_name = 'female89_100p_LNCC'  # Set this to None if you want to use the current timestamp
+start_time_string = custom_name if custom_name is not None else time.strftime("%Y-%m-%d-%H:%M:%S")
 
 # moving image (either or explicit frame name or None to pick the first one)
 moving_id = None
@@ -70,16 +71,27 @@ moving_id = None
 # dataset = '2023_05_10_test'
 dataset = 'images'
 
-
 # other data params
 wandb_usage = False
-os.environ['WANDB_API_KEY'] = 'xxx'    # wandb API key for your user
+os.environ['WANDB_API_KEY'] = '19df5b248f9a8b81b296738bc6b12c2d49cb9a6c'    # wandb API key for your user
 train_val_split = 0.80     # percentage of unsupervised training samples, rest is validation
 unsupervised_validation = True     # whether to use validation set without segmentations
 supervised_validation = False     # whether to use validation set with segmentations
 patient_specific_inference = False       # whether to use model trained on first frames of each patient in main_infer script
-inference = 'validation'   #  validation, testing 
+inference = 'validation'   # validation, testing
 
+# choose patients for labeled fine-tuning, validation, and testing set
+observer_training = ''
+patients_training = ['female89_100p']
+
+observer_validation = ''
+patients_validation = ['female89_100p']
+
+# observer_testing = 'contoured_ground_truth_VG'  #  contoured_ground_truth_LV, contoured_ground_truth_VG
+# patients_testing = ['liver_patient0006', 'liver_patient0027', 'abdomen_patient0008', 'abdomen_patient0017', \
+#                    'prostate_patient0006', 'prostate_patient0013', 'lung_patient0041', 'lung_patient0046', \
+#                    'pancreas_patient0002', 'pancreas_patient0011', \
+#                    'heart_patient0001', 'heart_patient0002', 'mediastinum_patient0002']
 
 
 # choose model for main script
@@ -87,7 +99,7 @@ model_name = 'TransMorph2D'  # TransMorph2D, BSpline, NoReg, InterObserver
 
 if model_name == 'TransMorph2D':
     model_variant = 'TransMorph'    # see dict_model_variants in models.TranMorph
-    load_state = False      # continue training with current best model/optimizer for epoch_nr epochs or load model for inferece
+    load_state = False      # continue training with current best model/optimizer for epoch_nr epochs or load model for inference
     if load_state: 
         # specify model 
         start_time_string = '2023-09-14-07:20:12'   # unsup --> this one has uploaded weights!
@@ -95,10 +107,10 @@ if model_name == 'TransMorph2D':
     batch_size = 2     # 2, 16, 64, 128, 192
     lr = 0.00001    # learning rate
     lr_scheduler = None   # None, WarmupCosine
-    epoch_nr = 7    # nr of training epochs   30, 50, 100, 150, 300, 500
-    loss_name = 'LNCC-Diffusion'   # MSE, SSIM, LNCC, Dice, MSE-Diffusion, MSE-Bending, LNCC-Diffusion, LNCC-Bending, Dice-Diffusion, Dice-Bending, MSE-Dice-Diffusion, MSE-Dice-Bending
-    loss_weights = [1, 0.01]   # weights for combined loss,  0th element is for image and 1st for displacmement part
-    # loss_weights = [0.05, 1, 0.005]   # weights for combined loss,  0th element is for image and 1st for segmentation and 2nd for displacmement part
+    epoch_nr = 1000    # nr of training epochs   30, 50, 100, 150, 300, 500
+    loss_name = 'LNCC'   # MSE, SSIM, LNCC, Dice, MSE-Diffusion, MSE-Bending, LNCC-Diffusion, LNCC-Bending, Dice-Diffusion, Dice-Bending, MSE-Dice-Diffusion, MSE-Dice-Bending
+    loss_weights = [1, 0.01]   # weights for combined loss,  0th element is for image and 1st for displacement part
+    # loss_weights = [0.05, 1, 0.005]   # weights for combined loss,  0th element is for image and 1st for segmentation and 2nd for displacement part
     prob_randaffine = 0.75
     prob_randelasticdef = 1.0  # must be 1.0
     prob_randmotion = 0.75
@@ -112,8 +124,8 @@ elif model_name == 'BSpline':
     print('Running on CPU!')
     lambda_value = '5.0'  # regularization parameter
     init_grid_spac = '90'  # grid spacing in mm of first B-Spline stage
-    metric='mse'   # mse, mi, gm
-    max_its='500'
+    metric = 'mse'   # mse, mi, gm
+    max_its = '500'
     impl = 'plastimatch'  # plastimatch, itk
             
 elif model_name == 'NoReg':
@@ -127,5 +139,3 @@ elif model_name == 'InterObserver':
     
 else:
     raise ValueError('Unknown model_name specified!')  
-
-#%%
